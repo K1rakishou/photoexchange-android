cda0b869cad1751a50259953be4f84d5ff08c2ed
kirakishou <kez1911@protonmail.com>
2018-04-22 19:23:51 +0300

    Added MyGlideModule with customized disk cache size

--- app/src/main/java/com/kirakishou/photoexchange/helper/ImageLoader.kt
+++ app/src/main/java/com/kirakishou/photoexchange/helper/ImageLoader.kt
@@ -2,48 +2,39 @@ package com.kirakishou.photoexchange.helper
 
 import android.content.Context
 import android.widget.ImageView
-import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.kirakishou.photoexchange.PhotoExchangeApplication
-import io.reactivex.Single
+import com.kirakishou.photoexchange.di.module.GlideApp
 import java.io.File
 import javax.inject.Inject
+
 
 /**
  * Created by kirakishou on 12/20/2017.
  */
 class ImageLoader
 @Inject constructor(
-        private val context: Context
+    private val context: Context
 ) {
     private val basePhotosUrl = "${PhotoExchangeApplication.baseUrl}v1/api/get_photo"
 
     fun loadImageFromDiskInto(imageFile: File, view: ImageView) {
-        Glide.with(context)
-                //we do not need to cache this image
-                .applyDefaultRequestOptions(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.NONE))
-                .load(imageFile)
-                .apply(RequestOptions().centerCrop())
-                .into(view)
+        GlideApp.with(context)
+            //we do not need to cache this image
+            .applyDefaultRequestOptions(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.NONE))
+            .load(imageFile)
+            .apply(RequestOptions().centerCrop())
+            .into(view)
     }
 
     fun loadImageFromNetInto(photoName: String, photoSize: PhotoSize, view: ImageView) {
         val fullUrl = "$basePhotosUrl/$photoName/${photoSize.value}"
-        Glide.with(context)
-                .load(fullUrl)
-                .apply(RequestOptions().centerCrop())
-                .into(view)
-    }
-
-    fun downloadPhotoAsync(photoName: String, photoSize: PhotoSize): Single<File> {
-        val fullUrl = "$basePhotosUrl/$photoName/${photoSize.value}"
-        val future = Glide.with(context)
-                .downloadOnly()
-                .load(fullUrl)
-                .submit()
-
-        return Single.fromFuture(future)
+        GlideApp.with(context)
+            .load(fullUrl)
+            .diskCacheStrategy(DiskCacheStrategy.DATA)
+            .apply(RequestOptions().centerCrop())
+            .into(view)
     }
 
     enum class PhotoSize(val value: String) {
